b4f10366b7f73e66834c15fc146e036d4b0d1bb5
DeivisZol <68916713+DeivisZol@users.noreply.github.com>
2021-12-18 04:07:30 +0200

    Java fx scene biuld
    Layout made and elements connected to the code

--- src/main/java/deivis/paymentsystem/HelloController.java
+++ src/main/java/deivis/paymentsystem/HelloController.java
@@ -1,14 +1,73 @@
 package deivis.paymentsystem;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.scene.control.Label;
+import javafx.fxml.Initializable;
+import javafx.scene.control.*;
 
-public class HelloController {
-    @FXML
-    private Label welcomeText;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class HelloController implements Initializable {
+
 
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+    public Button createGroupButton;
+    @FXML
+    public Button groupPaymentButton;
+    @FXML
+    public Button loadFromFileButton;
+    @FXML
+    public Button printToCvsFile;
+    @FXML
+    public Button printToPdfFile;
+    @FXML
+    public Button createStudentButton;
+    @FXML
+    public Button loadTableButton;
+
+    @FXML
+    public TextField createGroupName;
+    @FXML
+    public TextField groupMonthPayment;
+    @FXML
+    public TextField loadFromFileName;
+    @FXML
+    public TextField studentName;
+    @FXML
+    public TextField studentSurname;
+    @FXML
+    public TextField studentId;
+
+
+    @FXML
+    public ChoiceBox<String> groupPayment;
+    @FXML
+    public ChoiceBox<String> selectMonth;
+    @FXML
+    public ChoiceBox<String> selectGroup;
+
+    @FXML
+    private TableView<String> table;
+    @FXML
+    private TableColumn<String, Integer> tableId;
+    @FXML
+    private TableColumn<String, Integer> tableName;
+    @FXML
+    private TableColumn<String, Integer> tableSurname;
+    @FXML
+    private TableColumn<String, Integer> tableGroup;
+    @FXML
+    private TableColumn<String, Integer> tableMonth;
+    @FXML
+    private TableColumn<String, Integer> tablePaymentAmount;
+
+
+    public void pressButton(ActionEvent event) {
+        System.out.println("s");
+    }
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+
     }
 }
